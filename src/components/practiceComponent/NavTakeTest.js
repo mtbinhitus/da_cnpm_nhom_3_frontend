f35e55d0e6ce408c21942195a611f072dfcd6621
khanh.dinhtrong <19127437@student.hcmus.edu.vn>
2023-03-09 21:36:54 +0700

    Done UI screen to start the

---
+++ src/components/practiceComponent/NavTakeTest.js
@@ -0,0 +1,22 @@
+import {  Button} from "@mui/material";
+import "../../../src/style/practicePage.css"
+import ErrorOutlineIcon from '@mui/icons-material/ErrorOutline';
+
+const NavTakeTest = () => {
+
+    return (
+        <>
+            <div className="alert-warning">
+                <div className="tab-content" style={{ fontSize: "18px" }}>
+                    <span><ErrorOutlineIcon/></span>
+                    <span> Sẵn sàng để bắt đầu làm full test? Để đạt được kết quả tốt nhất, bạn cần dành ra 120 phút cho bài test này.</span>
+                </div>
+            </div>
+            <Button size="small" variant="contained" style={{ marginRight: "5px", backgroundColor: "#35509 !important", borderColor:" #35509a", fontSize: "18px", marginTop:"15px"}}>
+                BẮT ĐẦU THI
+            </Button>
+
+        </>
+    );
+};
+export default NavTakeTest
---
+++ src/components/practiceComponent/PracticeContent.js
@@ -0,0 +1,37 @@
+import { Tabs, Tab, Box, } from "@mui/material";
+import "../../../src/style/practicePage.css"
+import { useState } from "react";
+import NavTakeTest from "./NavTakeTest";
+import NavPractice from './NavPractice'
+const PracticeContent = () => {
+    const [value, setValue] = useState(0);
+
+    console.log(value)
+
+    return (
+        <>
+            <div>
+                <Box className="practice-content" sx={{ borderBottom: 1, borderColor: "divider", marginTop:"30px" }} style={{ color: "#8c98a4" }}>
+                    <Tabs
+                        textColor="blue"
+                        value={value}
+                        onChange={(e, value) => setValue(value)}
+                        aria-label="basic tabs example"
+                    >
+                        <Tab className={value === 0 ? "active" : null} style={{ textTransform: "none", fontSize: "16px", fontWeight: 'bold' }} label="Luyen Tap" />
+                        <Tab className={value && value === 1 ? "active" : null} style={{ textTransform: "none", fontSize: "16px", fontWeight: 'bold' }} label="Lam full test" />
+                        <Tab className={value && value === 2 ? "active" : null} style={{ textTransform: "none", fontSize: "16px", fontWeight: 'bold' }} label="Thao luan" />
+                    </Tabs>
+                </Box>
+               {
+                value===0?
+                <NavPractice/>:
+                value===1?
+                <NavTakeTest/>
+                :<div></div>
+               }
+            </div>
+        </>
+    );
+};
+export default PracticeContent;
