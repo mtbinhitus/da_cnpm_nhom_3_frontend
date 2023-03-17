d6e0002916bdbf3686dbfede9ef3763afa6ee670
khanh.dinhtrong <19127437@student.hcmus.edu.vn>
2023-03-17 15:16:05 +0700

    Update https call service

--- src/utils/request.js
+++ src/utils/request.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 const request = axios.create({
   // baseURL: "http://localhost:8080",
-  baseURL:"http://ets-service-production.up.railway.app"
+  baseURL:"https://ets-service-production.up.railway.app"
 });
 
 export default request;
