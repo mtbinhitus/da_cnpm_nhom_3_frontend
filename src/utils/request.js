a00bf6b23e399a7f208bf9b640d3d6831e159539
Binh Mai <mtbinhitus@gmail.com>
2023-03-17 12:29:20 +0700

    config packe-lock,json

--- src/utils/request.js
+++ src/utils/request.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const request = axios.create({
-  baseURL: "http://localhost:8080",
+  baseURL: "https://ets-service-production.up.railway.app/",
 //   baseURL:"linkdeploy"
 });
 
