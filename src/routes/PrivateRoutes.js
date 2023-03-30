5c225fed20b1bbe2a8f068420e7fd24e1937ae14
19127437 <64051785+19127437@users.noreply.github.com>
2023-03-30 22:51:00 +0700

    Create page admin create Exam

--- src/routes/PrivateRoutes.js
+++ src/routes/PrivateRoutes.js
@@ -2,13 +2,14 @@ import DetailPage from "../pages/DetailPage";
 import practicePage from "../pages/practicePage";
 import TestList from "../pages/TestList";
 import TestResult from "../pages/TestResult";
-
+import ManageExam from "../components/examManagement/ManageExam";
 const privateRoutes = [
     { path: "/exam-detail/:examId", element: practicePage },
     { path: "/exam-detail/:examId/start", element: DetailPage },
     { path: "/exam-detail/:examId/start", element: DetailPage },
     { path: "/exam-detail/:examId/results/:examResultId", element: TestResult },
     { path: "/exams", element: TestList },
+    { path: "/admin/exams", element: ManageExam },
 ];
 
 export { privateRoutes };
