0e09fea32ba0a8e06e951294fcb5419ea33f6d6d
rei202 <91878939+rei202@users.noreply.github.com>
2023-04-25 01:11:42 +0700

    update

--- src/components/practiceComponent/ChoicePractice.js
+++ src/components/practiceComponent/ChoicePractice.js
@@ -1,13 +1,17 @@
+import React from 'react';
 import Checkbox from '@mui/material/Checkbox';
 import '../../style/PracticePage.css';
+
 const ChoicePractice = (props) => {
     return (
         <>
             <Checkbox defaultChecked color="default" style={{ margin: 0 }} />
             <span style={{ fontSize: '18px' }}>{props?.name}</span>
             <div className="list-tag">
-                {props?.list?.map((data) => (
-                    <span className="tag name">#{data.name}</span>
+                {props?.list?.map((data, index) => (
+                    <span key={index} className="tag name">
+                        #{data.name}
+                    </span>
                 ))}
             </div>
         </>
