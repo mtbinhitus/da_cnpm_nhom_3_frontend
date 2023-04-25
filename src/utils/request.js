f0cf00ab0655a04ae8359a7b5b6e92d88341adb6
khanh.dinhtrong <19127437@student.hcmus.edu.vn>
2023-04-25 16:47:04 +0700

    Update Id locator

--- src/utils/request.js
+++ src/utils/request.js
@@ -1,8 +1,8 @@
 import axios from 'axios';
 
 const request = axios.create({
-    // baseURL: "http://localhost:8080",
-    baseURL: 'http://localhost:8081',
+    baseURL: 'http://localhost:8080',
+    // baseURL: 'http://localhost:8081',
     // baseURL: 'http://18.139.83.149:8080/',
     headers: {
         'Content-type': 'application/json; charset=UTF-8',
